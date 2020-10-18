9165eada4332df511cc3c23b8c0381eecab71c12
Henry Mortimer <henry@morti.net>
2020-10-18 22:45:38 +0100

    feat: add updater to main command

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,26 @@ import (
 	"os"
 
 	"github.com/hennersz/ConfSync/internal/sync"
+	"github.com/hennersz/ConfSync/internal/updater"
 )
 
 func main() {
 	syncer := sync.NewGitSyncer("https://github.com/hennersz/ConfSync.git", "./test")
 	err := syncer.Sync()
+
+	if err != nil {
+		fmt.Printf("Something went wrong: %v", err)
+		os.Exit(1)
+	}
+
+	u, err := updater.NewUpdater("./test")
+
+	if err != nil {
+		fmt.Printf("Something went wrong: %v", err)
+		os.Exit(1)
+	}
+
+	err = u.Update()
 
 	if err != nil {
 		fmt.Printf("Something went wrong: %v", err)
